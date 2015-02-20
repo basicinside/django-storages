2f1ed873a870d844972d4036aa537938b31afce7
Robin Kuck <robin@basicinside.de>
2015-02-20 05:12:45 +0100

    Add frankfurt workaround

--- storages/backends/s3boto.py
+++ storages/backends/s3boto.py
@@ -276,13 +276,8 @@ class S3BotoStorage(Storage):
     def connection(self):
         if self._connection is None:
             self._connection = self.connection_class(
-                self.access_key,
-                self.secret_key,
-                is_secure=self.use_ssl,
-                calling_format=self.calling_format,
-                host=self.host,
-                port=self.port,
-            )
+                self.access_key, self.secret_key,
+                calling_format=self.calling_format, host='s3.eu-central-1.amazonaws.com')
         return self._connection
 
     @property
